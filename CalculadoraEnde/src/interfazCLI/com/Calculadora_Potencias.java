87b1d1bddef8ff84caf6e0a53a5451a901e7f94d
israel <i.delamo@gmail.com>
2019-04-01 14:05:35 +0200

    Módulo de potencias con JUNIT
    El módulo de potencias sirve para calcular potencias de diferentes bases
    y expoentes, además puede devolver valores redondeados o enteros o
    float, según sobrecarga de funciones.

---
+++ CalculadoraEnde/src/interfazCLI/com/Calculadora_Potencias.java
@@ -0,0 +1,16 @@
+package interfazCLI.com;
+
+
+/***
+ * En esta clase se gestiona todo el sistema de cáculo de las potencias
+ * y sus diferentes excepciones (si las hubiera)
+ * 
+ * La clase está diseñada de modo que tanto en CLI como en GUI se puede
+ * llamar a sus métodos.
+ * Estos últimos los diseñará el alumno
+ * @author israel
+ *
+ */
+public class Calculadora_Potencias {
+
+}
---
+++ CalculadoraEnde/src/interfazCLI/com/Calculadora_PotenciasTest.java
@@ -0,0 +1,14 @@
+package interfazCLI.com;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Calculadora_PotenciasTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
