82f5ab0b4fd3d77de676034ed9d0f4a525d50332
israel <i.delamo@gmail.com>
2019-04-01 13:32:44 +0200

    Creadas las firmas de Sumar() y LanzarMenu()
    No hay nada implementado, y será labor de los alumnos
    el corregir y añadir lo que les haga falta.

---
+++ CalculadoraEnde/src/interfazCLI/com/CalculadoraCLI.java
@@ -0,0 +1,38 @@
+package interfazCLI.com;
+
+public class CalculadoraCLI {
+/**
+ * Es el programa principal de linea de comandos
+ * nuestra labor será extraer los métodos que haga falta y 
+ * en la medida de lo posible pasarlo a interfaz gráfico
+ * o al menos tenerlo funcionando
+ * @param args
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LanzarMenu();
+
+	}
+
+	
+	/**
+	 * Muestra un menú de linea de comando con diferentes opciones
+	 * como son meter un operador A, un operador B
+	 * y realziar una operacion sobre ellos
+	 */
+private static void LanzarMenu() {
+	System.out.println("Bienvenido a la calculadora");
+
+}
+
+
+private static int Sumar() {
+	System.out.println("Yo sumo dos operadores");
+	return 0;
+
+}
+
+
+
+
+}
